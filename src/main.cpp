247aa6453fb1cad274aee68545b2fc65dd874b30
root <root@AhmedAliMSoliman530S.localdomain>
2020-04-01 21:44:23 +0400

    code workthrough main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,22 @@
 #include "game.h"
 #include "renderer.h"
 
-int main() {
-  constexpr std::size_t kFramesPerSecond{60};
-  constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
+/*
+
+1. To begin, main creates a Controller, a Game, and a Renderer object. Game stores a Snake object as part of the state.
+2. main calls Game::Run to start the game loop.
+
+main.cpp
+This is the entrypoint for the program. The main function in this file sets variables such as the window height and width and the number of frames per 
+second at which the game will be played. The main also creates Renderer, Controller, and Game objects, and calls the Game::Run method to start the game 
+loop.
+
+*/
+
+int main() 
+{
+  constexpr std::size_t kFramesPerSecond{60};                     //Game running at 60 frame per second 
+  constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};     // milli second per frame 
   constexpr std::size_t kScreenWidth{640};
   constexpr std::size_t kScreenHeight{640};
   constexpr std::size_t kGridWidth{32};
@@ -15,6 +28,7 @@ int main() {
   Controller controller;
   Game game(kGridWidth, kGridHeight);
   game.Run(controller, renderer, kMsPerFrame);
+
   std::cout << "Game has terminated successfully!\n";
   std::cout << "Score: " << game.GetScore() << "\n";
   std::cout << "Size: " << game.GetSize() << "\n";
