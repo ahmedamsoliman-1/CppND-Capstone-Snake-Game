83dbe487bab36848643b221beb9bf6043bd5c41d
root <root@AhmedAliMSoliman530S.localdomain>
2020-04-01 21:45:37 +0400

    code walkthrough snake

--- src/snake.h
+++ src/snake.h
@@ -4,37 +4,36 @@
 #include <vector>
 #include "SDL.h"
 
-class Snake {
- public:
-  enum class Direction { kUp, kDown, kLeft, kRight };
+class Snake 
+{
+public:
+      enum class Direction { kUp, kDown, kLeft, kRight };
+      //constroctor - start the snake in middle of the screen
+      Snake(int grid_width, int grid_height)
+          : grid_width(grid_width),
+            grid_height(grid_height),
+            head_x(grid_width / 2),
+            head_y(grid_height / 2) {}
+      //methods
+      void Update();
+      void GrowBody();
+      bool SnakeCell(int x, int y);
 
-  Snake(int grid_width, int grid_height)
-      : grid_width(grid_width),
-        grid_height(grid_height),
-        head_x(grid_width / 2),
-        head_y(grid_height / 2) {}
+      Direction direction = Direction::kDown;
 
-  void Update();
+      float speed{0.1f};
+      int size{1};
+      bool alive{true};
+      float head_x;
+      float head_y;
+      std::vector<SDL_Point> body; //SDL_Point is struct that has x and y coordinent
 
-  void GrowBody();
-  bool SnakeCell(int x, int y);
-
-  Direction direction = Direction::kUp;
-
-  float speed{0.1f};
-  int size{1};
-  bool alive{true};
-  float head_x;
-  float head_y;
-  std::vector<SDL_Point> body;
-
- private:
-  void UpdateHead();
-  void UpdateBody(SDL_Point &current_cell, SDL_Point &prev_cell);
-
-  bool growing{false};
-  int grid_width;
-  int grid_height;
+private:
+      void UpdateHead();
+      void UpdateBody(SDL_Point &current_cell, SDL_Point &prev_cell);
+      bool growing{false};
+      int grid_width;
+      int grid_height;
 };
 
 #endif
