bbdf9d30c8cda46f500436a7898dedc0fa3f5a8a
root <root@AhmedAliMSoliman530S.localdomain>
2020-04-01 21:55:51 +0400

    code walk through renderer

--- src/renderer.h
+++ src/renderer.h
@@ -5,10 +5,10 @@
 #include "SDL.h"
 #include "snake.h"
 
-class Renderer {
+class Renderer 
+{
  public:
-  Renderer(const std::size_t screen_width, const std::size_t screen_height,
-           const std::size_t grid_width, const std::size_t grid_height);
+  Renderer(const std::size_t screen_width, const std::size_t screen_height, const std::size_t grid_width, const std::size_t grid_height);
   ~Renderer();
 
   void Render(Snake const snake, SDL_Point const &food);
